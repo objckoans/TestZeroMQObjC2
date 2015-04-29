0e12609b6ebc24d97003c23af76708e7ed37a368
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-29 16:05:55 +0800

    Integration works

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'TestZeroMQObjC2' do
+    pod "objc-zmq", :git => 'git@github.com:calvinchengx/objc-zmq.git'
+end
+
+target 'TestZeroMQObjC2Tests' do
+
+end
+
--- TestZeroMQObjC2/ViewController.m
+++ TestZeroMQObjC2/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ZMQObjC.h"
 
 @interface ViewController ()
 
@@ -14,8 +15,37 @@
 
 @implementation ViewController
 
+- (void) requestZMQ
+{
+    ZMQContext *ctx = [[ZMQContext alloc] initWithIOThreads:1];
+    NSString *endpoint = @"tcp://192.168.1.10:9900";
+    ZMQSocket *requester = [ctx socketWithType:ZMQ_REQ];
+
+    BOOL didConnect = [requester connectToEndpoint:endpoint];
+    if (!didConnect) {
+        NSLog(@"*** Failed to connect to endpoint [%@].", endpoint);
+    }
+    NSData *request = [@"13599832991039421,user1,123,getHistory,^XAUUSD,2,1,10,4EC514F8CDA44339A3367E7ECF53BC1E" dataUsingEncoding:NSUTF8StringEncoding];
+    
+    int kMaxRequest = 10;
+    for (int request_nbr = 0; request_nbr < kMaxRequest; ++request_nbr) {
+        
+        // send data
+        [requester sendData:request withFlags:0];
+        NSLog(@"Sending request %d", request_nbr);
+        
+        // receive data
+        NSData *reply = [requester receiveDataWithFlags:0];
+        NSString *text = [[NSString alloc] initWithData:reply encoding:NSUTF8StringEncoding];
+        NSLog(@"Received reply %d: %@", request_nbr, text);
+    }
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"Hello");
+    [self requestZMQ];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
